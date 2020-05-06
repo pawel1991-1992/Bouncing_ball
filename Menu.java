51c502a52e1a309e28f308e34ac22c40ec922bf5
pawel1991-1992 <pawel1991-1992@tlen.pl>
2020-05-06 13:34:29 +0200

    First commit of the program

---
+++ Menu.java
@@ -0,0 +1,142 @@
+import javax.swing.JFrame;
+import java.util.Observer;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JOptionPane;
+import java.awt.Dimension;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.Box;
+
+/**
+ * Write a description of class Macheads here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu extends JFrame implements ActionListener
+{
+    
+    
+    JFrame f1 = new JFrame();
+    JFrame f2 = new JFrame();
+    second s = new second();
+    int odp;
+
+    JMenuBar menuBar;
+    JMenu menuMenu, menuPomoc;
+    JMenuItem mNowaGra, mPauza, mResume, mExit, mInstrukcja;
+    private static JTextField display;
+    
+    
+    
+    
+    public Menu()
+    {
+        
+        f1.setVisible(true);
+        f1.setSize(800,600);
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setTitle("Moving Ball");
+        
+        
+        menuBar = new JMenuBar();
+        menuMenu = new JMenu("Menu");
+        menuPomoc = new JMenu("Pomoc");
+        
+        mNowaGra = new JMenuItem("Nowa gra");
+        mNowaGra.addActionListener(this);
+        mPauza = new JMenuItem("Pauza");
+        mPauza.addActionListener(this);
+        mResume = new JMenuItem("Wznów");
+        mResume.addActionListener(this);
+        mExit = new JMenuItem("Wyjście");
+        mExit.addActionListener(this);
+        mInstrukcja = new JMenuItem("Instrukcja");
+        mInstrukcja.addActionListener(this);
+        
+        menuMenu.add(mNowaGra);
+        menuMenu.add(mPauza);
+        menuMenu.add(mResume);
+        menuMenu.add(mExit);
+        menuPomoc.add(mInstrukcja);
+        
+        f1.setJMenuBar(menuBar);
+        menuBar.add(menuMenu);
+        menuBar.add(Box.createHorizontalGlue());
+        menuBar.add(menuPomoc);
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        Object z = e.getSource(); 
+
+        if(z==mNowaGra)
+        {
+            odp = JOptionPane.showConfirmDialog(null, "Level 1", "Gra", JOptionPane.DEFAULT_OPTION);
+            if(odp==JOptionPane.OK_OPTION)
+            {
+            NowaGra();
+            s.pauza=false;
+            }
+        else if (odp==JOptionPane.CLOSED_OPTION)
+        {
+            f2.dispose();
+            f1.setVisible(true);
+        }
+        }
+        
+        if(z==mPauza)
+        {
+            s.pauza=true;
+        }
+        if(z==mResume)
+        {
+            s.pauza=false;
+        }
+        if(z==mExit)
+        {
+           CloseMenu();
+        }
+        if(z==mInstrukcja)
+        {
+            JOptionPane.showMessageDialog(null,"Gra polega na odbijaniu piłki belką znajdującą się u dołu ekranu. Możemy nią sterować \n lewym przyciskiem myszy - przeciągamy nim  w w płaszczyźnie poziomiej belkę w kierunku jakim chcemy.\nNową Grę zaczynamy opcją w Menu, do przebycia jest 10 poziomów :-)","Instrukcja",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        Menu appMenu = new Menu();
+        
+        
+    }
+    public void NowaGra() 
+    {
+            f1.dispose();
+            f2.add(s);
+            f2.setSize(800,600);
+            f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            f2.setTitle("Moving Ball");
+            f2.setVisible(true);
+            f2.setJMenuBar(menuBar);
+            s.x=0;
+            s.y=0;
+    }
+    public void CloseMenu()
+    {
+        f1.dispose();
+        f2.dispose();
+    }
+    public void CloseGame()
+    {
+        f2.dispose();
+    }
+    
+}
---
+++ second.java
@@ -0,0 +1,190 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JOptionPane;
+
+/**
+ * Write a description of class second here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class second extends JPanel implements ActionListener, MouseListener,MouseMotionListener
+{
+    Timer t = new Timer(5, this);
+    double x=0,y=0,velX=2,velY=2;
+    double xx=1,yy=1;
+    int zliczak=0, ans;
+    boolean pauza=false;
+    Rectangle2D rectangle = new Rectangle2D.Double(xx,500,120,30);
+    
+    
+    
+    public second()
+    {
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        Ellipse2D circle = new Ellipse2D.Double(x,y,40,40);
+        g2.fill(circle);
+        g2.fill(rectangle);
+        t.start();
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if (x < 0 || x > 760) 
+        {
+            velX = - velX;
+            
+        }
+        if(y < 0)
+        {
+            velY = -velY;
+            
+        }
+        if(x>xx && x<xx+160)
+        {
+            if(y>460)
+            {
+             velY = -velY;
+             zliczak++;
+             if(zliczak==4)
+             {
+                 x=0;
+                 y=0;
+                 velX=3;
+                 velY=3;
+                 JOptionPane.showConfirmDialog(null, "Level 2", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==10)
+             {
+                 x=0;
+                 y=0;
+                 velX=4;
+                 velY=4;
+                 JOptionPane.showConfirmDialog(null, "Level 3", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==18)
+             {
+                 x=0;
+                 y=0;
+                 velX=5;
+                 velY=5;
+                 JOptionPane.showConfirmDialog(null, "Level 4", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==28)
+             {
+                 x=0;
+                 y=0;
+                 velX=6;
+                 velY=6;
+                 JOptionPane.showConfirmDialog(null, "Level 5", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==40)
+             {
+                 x=0;
+                 y=0;
+                 velX=7;
+                 velY=7;
+                 JOptionPane.showConfirmDialog(null, "Level 6", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==54)
+             {
+                 x=0;
+                 y=0;
+                 velX=8;
+                 velY=8;
+                 JOptionPane.showConfirmDialog(null, "Level 7", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==70)
+             {
+                 x=0;
+                 y=0;
+                 velX=9;
+                 velY=9;
+                 JOptionPane.showConfirmDialog(null, "Level 8", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==98)
+             {
+                 x=0;
+                 y=0;
+                 velX=10;
+                 velY=10;
+                 JOptionPane.showConfirmDialog(null, "Level 9", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+             else if(zliczak==118)
+             {
+                 x=0;
+                 y=0;
+                 velX=11;
+                 velY=11;
+                 JOptionPane.showConfirmDialog(null, "Level 10", "Gra", JOptionPane.DEFAULT_OPTION);
+             }
+            }
+        }
+        if(y>480)
+            {
+               ans=JOptionPane.showConfirmDialog(null, "GAME OVER \n Chcesz rozpocząć grę od nowa?", "Gra", JOptionPane.YES_NO_OPTION);
+               y=0;
+               if(ans==JOptionPane.YES_OPTION)
+               {
+                   ans=0;
+                   
+                   pauza=false;
+                   y=0;
+                   x=0;
+                   velX=2;
+                   velY=2;
+                   zliczak=0;
+                   JOptionPane.showConfirmDialog(null, "Level 1", "Gra", JOptionPane.DEFAULT_OPTION);
+               }
+               else
+               {
+                  Menu k = new Menu();
+                  k.CloseGame();
+                  ans=0;
+                  y=0;
+                  x=0;
+                  zliczak=0;
+                  pauza=true;
+               }
+            }
+        
+        if(pauza==false){
+        x += velX;
+        y += velY;
+        repaint();
+        }
+        
+    }
+    public void mouseDragged(MouseEvent e)
+    {
+        xx=e.getX()-80;
+        yy=e.getY();
+        rectangle.setFrame(xx,500,160,30);
+        repaint();
+    } 
+    public void mouseMoved(MouseEvent e)
+    {
+       
+    }
+    public void mouseClicked(MouseEvent e){}
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e) {}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+}
